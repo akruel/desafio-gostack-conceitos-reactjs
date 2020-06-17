1b395ead947e211e947f8e7c57c745ac82dbd7fa
Anderson Kruel <anderson.kruel@gmail.com>
2020-06-16 23:48:30 -0300

    desafio concluído

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./styles.css";
+import './styles.css';
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  const newRepository = {
+    title: 'Desafio GoStack',
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+    url: 'https://github.com/akruel/desafio-gostack-conceitos-nodejs',
+  };
+
+  useEffect(() => {
+    api
+      .get('repositories')
+      .then((response) => {
+        const repositories = response.data;
+        console.log(repositories);
+        setRepositories(repositories);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api
+      .post('repositories', newRepository)
+      .then((response) => {
+        console.log(response);
+        if (response.status) {
+          setRepositories([...repositories, response.data]);
+        }
+      })
+      .catch((error) => console.error(error));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api
+      .delete(`repositories/${id}`)
+      .then((response) => {
+        if (response.status) {
+          const newRepositories = repositories.filter(
+            (repository) => repository.id !== id,
+          );
+          setRepositories(newRepositories);
+        }
+      })
+      .catch((error) => console.error(error));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
